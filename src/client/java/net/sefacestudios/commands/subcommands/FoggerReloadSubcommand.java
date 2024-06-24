37e25f9f346e78e3c7506f16097a6912b6605d72
Leonardo Luiz Gava <llgavamt@gmail.com>
2024-06-24 19:54:22 -0400

    update: fogpack.schema.json

--- src/client/java/net/sefacestudios/commands/subcommands/FoggerReloadSubcommand.java
+++ src/client/java/net/sefacestudios/commands/subcommands/FoggerReloadSubcommand.java
@@ -2,7 +2,6 @@ package net.sefacestudios.commands.subcommands;
 
 import com.mojang.brigadier.context.CommandContext;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
-import net.minecraft.text.Text;
 import net.sefacestudios.config.FoggerConfig;
 import net.sefacestudios.fogpack.Fogpack;
 import net.sefacestudios.fogpack.FogpackManager;
